f22bc75adda2fd84e93344ccf94ac5d5a310d546
sepehr310 <w3sepehr79@gmail.com>
2024-05-30 18:29:44 +0330

    add Read and Write File IO

--- Branches/branches.service.cs
+++ Branches/branches.service.cs
@@ -1,23 +1,29 @@
 //using Newtonsoft.Json;
+
+using DataManagement;
+
 namespace Branches;
 
 public class BranchesService
 {
-    private List<BranchesSchema> branches = new List<BranchesSchema>();
     public void create_Branch(BranchesSchema branch)
     {
+     List<BranchesSchema> branches = new List<BranchesSchema>();
 
-        branches.Add(branch);
+     branches.Add(branch);
 
-    
-        
+     DataManagementService.save_data(branches,"branches.json");
+
+
     }
     
     public void findAll()
     {
+        List<BranchesSchema> branches = DataManagementService.get_data<List<BranchesSchema>>("branches.json");
         foreach (var variable in branches)
         {
             Console.WriteLine(variable.branchName);
+            
         }
     }
 
---
+++ DataManagement/dataManagementService.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+//using System.Text.Json;
+
+namespace DataManagement;
+public class DataManagementService
+{
+    public static bool save_data<T>(T data,string filename)
+    {
+        
+        try
+        {
+            string filePath = $"../../../Data/{filename}";
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            string directoryPath = Path.GetDirectoryName(filePath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            
+            Console.WriteLine(directoryPath);
+            
+            File.WriteAllText(filePath, json,Encoding.UTF8);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+    }
+
+    public  static  T  get_data<T>(string filename)
+    {
+        try
+        {
+            string filePath = $"../../../Data/{filename}";
+            string directoryPath = Path.GetDirectoryName(filePath);
+           var stringData=  File.ReadAllText(directoryPath,Encoding.UTF8);
+           T data = JsonConvert.DeserializeObject<T>(stringData);
+           return data;        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception(e.Message);
+        }
+    }
+}
